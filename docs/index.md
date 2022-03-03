48def807e5b4a0700015192ad01695c36fabab57
W3113N <tim.wellen@gmx.net>
2022-03-03 09:09:55 +0000

    Adding a graph

--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,3 @@
 This is my page. I'm a Master Sudent in Finance at Nova SBE. Hello. 
+
+I'm building my first GitHub Website
--- docs/test.fsx
+++ docs/test.fsx
@@ -2,3 +2,28 @@
 ## This is my Page (Edited)
 *)
 
+
+#load "YahooFinance.fsx"
+
+
+open YahooFinance
+
+let twnk = YahooFinance.PriceHistory("TWNK")
+
+twnk.[..3]
+
+(***include-it***)
+
+
+#r "nuget: Plotly.NET, 2.0.0-preview.17"
+
+open Plotly.NET
+
+twnk // Prices List
+
+|> List.map (fun day -> day.Date, day.AdjustedClose)
+|>Chart.Line
+|>GenericChart.toChartHTML
+
+
+(***include-it-raw***)
